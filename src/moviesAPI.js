7330db215799920468c076e4005af374109dafb6
OlesiaManina <113991075+OlesiaManina@users.noreply.github.com>
2023-05-30 00:32:41 +0300

    changed

--- src/moviesAPI.js
+++ src/moviesAPI.js
@@ -1,38 +1,30 @@
 import axios from "axios";
 
-const BASE_URL = 'https://api.themoviedb.org/3';
-const API_KEY = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNDEzM2IxYjZmYjFlNTlkN2IxNjNjYWE0ZWVjNTBhZSIsInN1YiI6IjY0NzIxMzk2OWFlNjEzMDEyNTdiYzY1MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tcNdIoje0HBWD-U1jl32B-4ZQW7MTvc6gqKuda04J_Q';
+axios.defaults.baseURL = 'https://api.themoviedb.org/';
+const API_KEY = '24133b1b6fb1e59d7b163caa4eec50ae';
 
-const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: API_KEY
-    }
-  };
-  
- async function fetchTranding () {
-    const response = await axios.get(`${BASE_URL}/trending/movie/day?language=en-US`, options);
+async function fetchTranding () {
+    const response = await axios.get(`3/trending/movie/day?api_key=${API_KEY}`);
     return response.data.results;
 }
 
 async function fetchDetailInfo (movie_id) {
-    const response = await axios.get(`${BASE_URL}/movie/${movie_id}?language=en-US`, options);
-    return response;
+  const response = await axios.get(`3/movie/${movie_id}?api_key=${API_KEY}`);
+  return response;
 }
 
 async function fetchCastInfo (movie_id) {
-  const response = await axios.get(`${BASE_URL}/movie/${movie_id}/credits?language=en-US`, options);
+  const response = await axios.get(`3/movie/${movie_id}/credits?api_key=${API_KEY}`);
   return response;
 }
 
 async function fetchReviews (movie_id) {
-  const response = await axios.get(`${BASE_URL}/movie/${movie_id}/reviews?language=en-US`, options);
+  const response = await axios.get(`3/movie/${movie_id}/reviews?api_key=${API_KEY}`);
   return response;
 }
 
 async function fetchBySearch (value) {
-  const response = await axios.get(`${BASE_URL}/search/movie?query=${value}`, options);
+  const response = await axios.get(`3/search/movie?api_key=${API_KEY}&query=${value}`);
   return response;
 }
 
