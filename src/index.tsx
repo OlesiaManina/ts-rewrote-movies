74c4128f291e8a17694bd2b9978674c7bad03cc5
OlesiaManina <113991075+OlesiaManina@users.noreply.github.com>
2023-11-23 19:23:07 +0200

    changed tsconfig.json

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import { App } from './components/App';
 import './index.css';
-  // "homepage": "https://OlesiaManina.github.io/goit-react-hw-05-movies/",
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
